57b327a236cf1f8c206981ec35a28770ac954aa0
m0vls <rakutin.vm@gmail.com>
2026-06-02 00:57:53 +0500

    some changes 2

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 const pool = new pg.Pool({
   host: process.env.DB_POSTGRES_HOST,
-  port: parseInt(process.env.DB_PORT ?? "443", 10),
+  port: parseInt(process.env.DB_PORT ?? "6543", 10),
   database: process.env.DB_POSTGRES_DATABASE,
   user: process.env.DB_POSTGRES_USER,
   password: process.env.DB_POSTGRES_PASSWORD,
